5c17c507530895fb6ade33d6bc5f73566e968f75
Piyushg104 <gayakwadp72@gmail.com>
2026-04-21 17:58:27 +0530

    Sum

---
+++ src/com/java/core/Sum.java
@@ -0,0 +1,17 @@
+package com.java.core;
+
+public class Sum {
+public static void main(String[] args) {
+	
+	int A=20;
+	int B=30;		
+	
+	int sum = A+B;
+	
+	System.out.println("sum is:"+sum);
+}
+	
+	
+	
+	
+}
--- src/com/java/core/Test.java
+++
@@ -1,7 +0,0 @@
-package com.java.core;
-
-public class Test {
-public static void main(String[] args) {
-	System.out.println("Piyush Gaikwad");
-}
-}
