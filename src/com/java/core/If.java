75bec29965b4e7dfe200f34fe4f9ae35d46643ed
Piyushg104 <gayakwadp72@gmail.com>
2026-04-24 00:24:27 +0530

    If and else (even-odd num)

---
+++ src/com/java/core/If.java
@@ -0,0 +1,23 @@
+package com.java.core;
+
+public class If {
+
+	public static void main(String[] args) {
+	int A = 18;
+			
+	if(A%2==0) {
+	System.out.println("This is even number:");	}
+	
+	else {
+		System.out.println("THis is odd number:");
+	}
+		
+	}
+	
+}
+	
+	
+	
+	
+	
+
